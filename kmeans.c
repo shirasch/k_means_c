980207d04252224737164366e5a5e73e06708bf5
shira <shneider1@mail.tau.ac.il>
2024-07-13 14:42:53 +0300

    some more functions

--- kmeans.c
+++ kmeans.c
@@ -12,15 +12,27 @@ struct vector {
     struct cord *cords;
 };
 
-void initialize_m_array(struct vector *head_vec, struct vector **vector_array, int K) {
-    struct vector *current = head_vec;
-    int i = 0;
+struct vector* duplicate_vector(struct vector *original) {
+    struct vector *dup = malloc(sizeof(struct vector));
+    struct cord *orig_cord = original->cords;
+    struct cord *dup_cord_head = NULL, *dup_cord_curr = NULL;
 
-    while (current != NULL && i < K) {
-        vector_array[i] = current;
-        current = current->next;
-        i++;
+    while (orig_cord != NULL) {
+        struct cord *new_cord = malloc(sizeof(struct cord));
+        new_cord->value = orig_cord->value;
+        new_cord->next = NULL;
+        if (dup_cord_head == NULL) {
+            dup_cord_head = new_cord;
+            dup_cord_curr = dup_cord_head;
+        } else {
+            dup_cord_curr->next = new_cord;
+            dup_cord_curr = new_cord;
+        }
+        orig_cord = orig_cord->next;
     }
+    dup->cords = dup_cord_head;
+    dup->next = NULL;
+    return dup;
 }
 
 void print_vectors(struct vector *head_vec) {
@@ -37,6 +49,21 @@ void print_vectors(struct vector *head_vec) {
         printf("\n");
 
         current_vector = current_vector->next;
+    }
+}
+void print_vector_array(struct vector **vector_array, int num_vectors) {
+    for (int i = 0; i < num_vectors; i++) {
+        struct vector *current_vector = vector_array[i];
+        printf("Duplicated Vector %d: ", i + 1);
+        while (current_vector != NULL) {
+            struct cord *current_cord = current_vector->cords;
+            while (current_cord != NULL) {
+                printf("%.5lf ", current_cord->value);
+                current_cord = current_cord->next;
+            }
+            current_vector = current_vector->next;
+        }
+        printf("\n");
     }
 }
 
@@ -89,6 +116,45 @@ struct vector* getinput(char *input_file, int *num_vectors) {
     return head_vec;
 }
 
+double calculate_distance(struct vector *vec1, struct vector *vec2) {
+    struct cord *cord1 = vec1->cords;
+    struct cord *cord2 = vec2->cords;
+    double sum = 0.0;
+
+    while (cord1 != NULL && cord2 != NULL) {
+        double diff = cord1->value - cord2->value;
+        sum += diff * diff;
+        cord1 = cord1->next;
+        cord2 = cord2->next;
+    }
+
+    if (cord1 != NULL || cord2 != NULL) {
+        fprintf(stderr, "Error: Vectors have different lengths.\n");
+        exit(1);
+    }
+
+    return sqrt(sum);
+}
+
+void find_closest_vectors(struct vector **vector_array, int num_vectors, struct vector **m_array, int K, int *closest_indices) {
+    for (int i = 0; i < num_vectors; i++) {
+        struct vector *current_vec = vector_array[i];
+        int closest_index = -1;
+        double min_distance = INFINITY;
+
+        for (int j = 0; j < K; j++) {
+            struct vector *m_vec = m_array[j];
+            double distance = calculate_distance(current_vec, m_vec);
+            if (distance < min_distance) {
+                min_distance = distance;
+                closest_index = j;
+            }
+        }
+
+        closest_indices[i] = closest_index;
+    }
+}
+
 int main(int argc, char **argv) {
     if (argc < 3) {
         fprintf(stderr, "An Error Has Occurred", argv[0]);
@@ -106,14 +172,33 @@ int main(int argc, char **argv) {
     }
 
 
-    // Create array of K vectors
-    struct vector **m_array = malloc(K * sizeof(struct vector *));
-    initialize_m_array(head_vec, m_array, K);
+
 
     struct vector **vector_array = malloc(num_vectors * sizeof(struct vector *));
-    
-    // print_vectors(head_vec);
-    
+    struct vector *current = head_vec;
+    for (int i = 0; i < num_vectors; i++) {
+        if (current == NULL) {
+            fprintf(stderr, "Error: Insufficient vectors in input file.\n");
+            return 1;
+        }
+        vector_array[i] = current;
+        current = current->next;
+    }
+
+    // initilize array of K duplicated vectors
+    struct vector **m_array = malloc(K * sizeof(struct vector *));
+    for (int i = 0; i < K; i++) {
+        if (i >= num_vectors) {
+            fprintf(stderr, "Error: K is greater than the number of vectors.\n");
+            return 1;
+        }
+        m_array[i] = duplicate_vector(vector_array[i]);
+    }
+
+    int *closest_indices = malloc(num_vectors * sizeof(int));
+
+    // Find the closest vectors and store their indices
+    find_closest_vectors(vector_array, num_vectors, m_array, K, closest_indices);
 
 
     // Free memory
@@ -130,7 +215,23 @@ int main(int argc, char **argv) {
         free(temp_vec);
     }
 
+    for (int i = 0; i < K; i++) {
+        struct vector *dup_vec = m_array[i];
+        while (dup_vec != NULL) {
+            temp_vec = dup_vec;
+            dup_vec = dup_vec->next;
+            struct cord *temp_cord;
+            while (temp_vec->cords != NULL) {
+                temp_cord = temp_vec->cords;
+                temp_vec->cords = temp_vec->cords->next;
+                free(temp_cord);
+            }
+            free(temp_vec);
+        }
+    }
+
     free(vector_array);
+    free(m_array);
 
     return 0;
 }
